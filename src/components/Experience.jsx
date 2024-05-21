5cee2d1553f29043072b15ed2643d418f49d8581
emmz3230 <emmz3230@gmail.com>
2024-05-21 11:45:00 +0100

    code, image, and gltf file

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,25 @@
-import { OrbitControls } from "@react-three/drei";
+import { useTexture, Decal, OrbitControls } from "@react-three/drei";
+import { Texture } from "three";
+// import { Mug } from "./Mug";
 
 export const Experience = () => {
+  const texture = useTexture("/textures/myPic.jpg");
   return (
     <>
       <OrbitControls />
       <mesh>
         <boxGeometry />
         <meshNormalMaterial />
+        <Decal
+          debug
+          position={[0, 0, 1]}
+          rotation={[0, 0, 0]}
+          scale={1}
+          polygonOffset
+          polygonOffsetFactor={-1}
+        >
+          <meshBasicMaterial map={texture} />
+        </Decal>
       </mesh>
     </>
   );
